18655ca200088ffd2b8943e358c9f9f9f8f187e0
vlorian <vlorian.dervishi@gmail.com>
2023-07-02 16:50:04 -0400

    movedDodger to the left

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Your code here
 const dodger=document.getElementById("dodger");
 
-dodger.style.backgroundColor="#FF69B4"
-dodger.style.bottom="0px";
-dodger.style.left="0px";
+function moveDodgerLeft(){
+  const leftNumbers=dodger.style.left.replace("px","");
+  const left = parseInt(leftNumbers,10);
+  if(left>0){
+    dodger.style.left=`${left-1}px`;
+  }
+}
+
+document.addEventListener("keydown",function(e){
+  if(e.key==="ArrowLeft"){
+    moveDodgerLeft();
+  }
+});
